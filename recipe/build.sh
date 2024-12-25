cd63bf45c6d57ead04721ce07cdf586c0cf031aa
yhori <yhori991@gmail.com>
2024-12-25 16:38:14 +0000

    Fix python3.13

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,15 +7,5 @@ cargo-bundle-licenses \
   --format yaml \
   --output ${SRC_DIR}/THIRDPARTY.yml
 
-## Apply PEP517 to install the package
-#maturin build \
-#  --release \
-#  --strip \
-#  --manylinux off \
-#  --interpreter="${PYTHON}"
-#
-## Install wheel manually
-#cd target/wheels
-#"${PYTHON}" -m pip install *.whl -vv --no-deps
-
+# Install package
 ${PYTHON} -m pip install . -vv
